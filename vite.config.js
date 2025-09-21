e7e9a29d2575868049c24c478e5e846abcc10d95
Armando Huerta Hoffmann <arhuertah@outlook.com>
2025-09-21 09:40:45 -0500

    Added fix to github pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/kids-tasks/', // Add base URL for GitHub Pages
+  base: '/kids-tasks/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+  }
 })
